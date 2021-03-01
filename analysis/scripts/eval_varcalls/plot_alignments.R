1aead0b2db585f36389121b9cfdb06ee592e74f9
Brice Letcher <bletcher@ebi.ac.uk>
2021-03-01 16:11:15 +0000

    Functional eval_varcalls for bowtie2 and varifier
    * Add a 'dummy' run of bowtie2-mapping based validation with an empty vcf
    * Produce one stats.tsv for bowtie2-mapping validation compiling .sam files for multiple tools
    * Add plotting R script
    * Update download_data container and container definition

---
+++ analysis/scripts/eval_varcalls/plot_alignments.R
@@ -0,0 +1,24 @@
+library(ggplot2)
+library(tibble)
+library(tidyr)
+library(dplyr)
+library(readr)
+library(forcats)
+library(argparser, quietly=TRUE)
+
+p <- arg_parser("Plot alignments")
+p <- add_argument(p, "stats_file", help="")
+p <- add_argument(p, "outdir", help="")
+argv <- parse_args(p)
+df = read_tsv(argv$stats_file)
+
+means <- df %>% drop_na() %>% group_by(gene, tool) %>% summarise(mean_NM = mean(NM))
+write_tsv(means,file.path(argv$outdir, "mean_NM.tsv"))
+
+p <- ggplot(df, aes(x=NM)) + geom_histogram(binwidth=0.005)
+p + facet_grid(rows=vars(gene),cols=vars(tool))
+ggsave(file.path(argv$outdir, "R_NM_facetgrid.pdf"), width=15, height=12)
+
+NAs<- df %>% group_by(tool, gene) %>% summarise(num_no_mapping=sum(is.na(NM)))
+ggplot(NAs, aes(y=num_no_mapping,x=gene,fill=tool)) + geom_col(position="dodge")
+ggsave(file.path(argv$outdir, "R_num_no_mapping.pdf"), width=14, height=9)
--- analysis/scripts/eval_varcalls/process_alignments.py
+++ analysis/scripts/eval_varcalls/process_alignments.py
@@ -1,10 +1,6 @@
 """
-Computes stats from sam alignments: NM (edit distance between query and target), AS
-(alignment score), MAPQ (probability that alignment is not unique).
-
-And writes the stats to a tsv file.
+See main() docstring for explanation
 """
-
 from collections import namedtuple
 from typing import Dict, Tuple, List
 from pathlib import Path
@@ -28,7 +24,7 @@ def load_mask_bed(mask_bed: click.Path) -> MaskDict:
     """
     Loads the amount of overlap between an aligned region and a mask
     of low-quality regions.
-    The key to the returned dict is a composite of alignment query, target and condition.
+    The key to the returned dict is a composite of alignment query, target and tool.
     """
     result = dict()
     if mask_bed is not None:
@@ -41,11 +37,10 @@ def load_mask_bed(mask_bed: click.Path) -> MaskDict:
     return result
 
 
-def get_sample_and_condition_name(sam_fname: Path) -> Tuple[str, str]:
-    elements = sam_fname.stem.split("_")
-    sample = elements[-1]
-    condition = "_".join(elements[:-1])
-    return sample, condition
+def get_sample_and_tool_name(sam_fname: Path) -> Tuple[str, str]:
+    sample = sam_fname.stem
+    tool = sam_fname.parent.name
+    return sample, tool
 
 
 def load_gene_lengths(bed_fname: Path) -> Dict[str, int]:
@@ -53,15 +48,21 @@ def load_gene_lengths(bed_fname: Path) -> Dict[str, int]:
     with bed_fname.open() as fin:
         for line in fin:
             cols = line.split("\t")
-            result[cols[3].strip()] = int(cols[2]) - int(cols[1]) + 1
+            gene_name = get_gene_name(cols[3].strip())
+            result[gene_name] = int(cols[2]) - int(cols[1]) + 1
     return result
+
+
+def get_gene_name(gene_string: str):
+    """Converts 'Name=<x>;ID=<y>' into <x>"""
+    return gene_string.split(";")[0].split("=")[1]
 
 
 def get_scores(sam_fname: Path, gene_lengths: Dict[str, int]) -> GeneScores:
     result: GeneScores = dict()
     samfile = AlignmentFile(sam_fname, "r")
     for read in samfile.fetch(until_eof=True):
-        gene_name = read.query_name
+        gene_name = get_gene_name(read.query_name)
         if gene_name in result:
             raise MultipleAlignmentsError(
                 f"Several alignments to gene {gene_name} in file {sam_fname}"
@@ -108,46 +109,31 @@ def write_stats(
     sam_file_list: List[Path], output_stats: Path, gene_lengths, mask_overlaps: MaskDict
 ):
 
-    # First pass: get baseline scores
-    baseline_scores: BaselineScores = dict()
-    for sam_fname in sam_file_list:
-        sample, condition = get_sample_and_condition_name(sam_fname)
-        if condition == "baseline_ref":
-            baseline_scores[sample] = get_scores(sam_fname, gene_lengths)
-
-    # Second pass: get scores and deltas relative to baseline
     with output_stats.open("w") as stats_file:
         fieldnames = [
             "sample",
             "gene",
-            "condition",
+            "tool",
             "NM",
             "AS",
             "MAPQ",
-            "delta_NM",
-            "delta_AS",
-            "delta_MAPQ",
             "mask_overlap",
         ]
         stats_file.write("\t".join(fieldnames) + "\n")
 
         for sam_fname in sam_file_list:
-            sample, condition = get_sample_and_condition_name(sam_fname)
+            sample, tool = get_sample_and_tool_name(sam_fname)
             scores = get_scores(sam_fname, gene_lengths)
-            delta_scores = get_delta_scores(baseline_scores[sample], scores)
-            for gene in delta_scores:
-                mask_key = f"{gene}_{sample}_{condition}"
+            for gene in scores:
+                mask_key = f"{gene}_{sample}_{tool}"
                 mask_overlap = mask_overlaps.get(mask_key, 0)
                 row = [
                     sample,
                     gene,
-                    condition,
+                    tool,
                     scores[gene].NM,
                     scores[gene].AS,
                     scores[gene].MAPQ,
-                    delta_scores[gene].NM,
-                    delta_scores[gene].AS,
-                    delta_scores[gene].MAPQ,
                     mask_overlap,
                 ]
                 stats_file.write("\t".join(map(str, row)) + "\n")
@@ -162,13 +148,26 @@ def write_stats(
 @click.argument("output_file", type=str)
 @click.option("--mask_bed", type=click.Path(exists=True), default=None)
 def main(input_dir, input_bed, output_file, mask_bed):
+    """
+    Computes stats from sam alignments and writes the stats to a tsv file. Computed stats include: NM (edit distance between query and target, scaled by gene length), AS
+    (alignment score), MAPQ (probability that alignment is not unique).
+
+
+    INPUT_DIR should be a directory containing other directories, each of which contain .sam files. Each sam-containing directory is listed as a tool in the output file.",
+    """
     output_file = Path(output_file).resolve()
     output_file.parent.mkdir(exist_ok=True)
 
     if not output_file.exists():
-        sam_file_list = list(Path(input_dir).glob(f"*.sam"))
+        sam_file_list = list()
+        for nextpath in Path(input_dir).glob("*"):
+            if not nextpath.is_dir():
+                if nextpath.suffix == ".sam":
+                    sam_file_list.append(nextpath)
+            else:
+                sam_file_list.extend(Path(nextpath).glob(f"*.sam"))
         if len(sam_file_list) == 0:
-            print(f"Error: no .sam files in {input_dir}")
+            print(f"Error: no .sam files under {input_dir}")
             exit(1)
 
         gene_lengths = load_gene_lengths(Path(input_bed))
