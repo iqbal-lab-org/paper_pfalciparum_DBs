4dc30823d1301aad464e6dd47611dd82c66d601c
Brice Letcher <bletcher@ebi.ac.uk>
2021-03-05 15:56:02 +0000

    First joint_genotyping workflow and refactored configuration
    * Create joint_genotyping workflow
    * Move download_data configuration and utils to common configuration and utils: loading sample tsvs
    * Move make_prgs utils to common utils: load a bed, get sample names from a dataset name

--- analysis/workflows/common_utils.py
+++ analysis/workflows/common_utils.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from typing import List
+from csv import DictReader
 from subprocess import run as sp_run, PIPE
 from glob import glob
+from itertools import product
 
 
 def get_gmtools_commit(container: str):
@@ -32,6 +34,12 @@ def get_gmtools_commit(container: str):
     return GMTOOLS_COMMIT
 
 
+def mk_output_dirs(variables):
+    """For each variable starting with 'output', makes the directory name it holds"""
+    for variable in filter(lambda name: name.startswith("output"), variables):
+        Path(eval(variable)).mkdir(exist_ok=True, parents=True)
+
+
 def gene_list_to_genome(wildcards):
     if "pf6" in wildcards.gene_list_name:
         return "Pfalciparum"
@@ -50,17 +58,19 @@ def get_assembly(wildcards):
     return assembly
 
 
-def mk_output_dirs(variables):
-    """For each variable starting with 'output', makes the directory name it holds"""
-    for variable in filter(lambda name: name.startswith("output"), variables):
-        Path(eval(variable)).mkdir(exist_ok=True, parents=True)
-
-
 def get_reads(wildcards):
-    return [
-        f'{config["dl_output_dir"]}/{wildcards.dataset_name}/{wildcards.sample_name}/reads_{i}.final.fastq.gz'
-        for i in [1, 2]
-    ]
+    if wildcards.dataset_name.startswith("pf6"):
+        return [
+            f'{config["dl_output_dir"]}/pf6/{wildcards.sample_name}/reads_{i}.final.fastq.gz'
+            for i in [1, 2]
+        ]
+    elif wildcards.dataset_name.startswith("pvgv"):
+        return [
+            f'{config["dl_output_dir"]}/pvgv/{wildcards.sample_name}/reads_{i}.final.fastq.gz'
+            for i in [1, 2]
+        ]
+    else:
+        raise ValueError(f"Support for {wildcards.dataset_name} not implemented")
 
 
 def get_ref_genome(wildcards):
@@ -71,6 +81,97 @@ def get_ref_genome(wildcards):
         "pacb_ilmn_pf": "Pfalciparum",
     }
     return f'{config["dl_output_dir"]}/ref_genomes/{ds_to_ref[wildcards.dataset_name]}.genome.fasta.gz'
+
+
+def get_sample_names(dataset_name):
+    if dataset_name == "pf6_analysis_set":
+        pf6_analysis_samples = load_pf6(config["pf6_tsv"], use_analysis_set=True)
+        return [rec.sample_name for rec in pf6_analysis_samples]
+    else:
+        raise ValueError(f"Support for {dataset_name} not implemented")
+
+
+def load_bed(gene_list_name):
+    bed_fname = f'{config["gene_bed_dir"]}/{gene_list_name}.bed'
+    result = list()
+    with open(bed_fname) as fin:
+        for line in fin:
+            name = line.split("\t")[3]
+            result.append(name)
+    return result
+
+
+##################
+# Load sample tsvs#
+##################
+class ENARecord:
+    DATASETS = {"pf6": "23.33Mb", "pvgv": "29.05Mb", "pacb_ilmn_pf": "23.33Mb"}
+
+    def __init__(self, dataset_name: str, sample_name: str, ena_IDs: str):
+        if dataset_name not in self.DATASETS:
+            raise ValueError(f"{dataset_name} not in {self.DATASETS}")
+        self.dataset_name = dataset_name
+        self.sample_name = sample_name
+        self.ena_IDs = ena_IDs
+
+
+def get_genome_size(dataset_name: str):
+    return ENARecord.DATASETS[dataset_name]
+
+
+def load_pf6(
+    tsv_fname: str, ignored_pattern: str = "", use_analysis_set: bool = False
+) -> List[ENARecord]:
+    ignored_samples = set()
+    ignored_file = Path(tsv_fname).parent / "ignored_samples.tsv"
+    with open(ignored_file) as tsvfile:
+        reader = DictReader(tsvfile, delimiter="\t")
+        for row in reader:
+            if ignored_pattern == "" or row["Reason"].startswith(ignored_pattern):
+                ignored_samples.add(row["Sample"])
+    result = list()
+    with open(tsv_fname) as tsvfile:
+        reader = DictReader(tsvfile, delimiter="\t")
+        for row in reader:
+            # Skips the Indonesian samples, which were blocked from public release so have no ENA IDs,
+            # and any other ignored samples (see the tsv for reason)
+            if row["ENA"] == "" or row["Sample"] in ignored_samples:
+                continue
+            # Skip non analysis_set pf6 samples if asked for
+            if use_analysis_set and row["Exclusion reason"] != "Analysis_set":
+                continue
+            result.append(ENARecord("pf6", row["Sample"], row["ENA"]))
+    return result
+
+
+def load_pvgv(tsv_fname: str) -> List[ENARecord]:
+    result = list()
+    with open(tsv_fname) as tsvfile:
+        reader = DictReader(tsvfile, delimiter="\t")
+        for row in reader:
+            ena_IDs = row["Sequencing Reads"].split(";")
+            # This skips the Indonesian samples, which were blocked from public release so have no ENA IDs
+            if all(map(lambda ID: ID == "", ena_IDs)):
+                continue
+            ena_IDs = ",".join(map(lambda s: s.strip(), ena_IDs))
+            result.append(ENARecord("pvgv", row["Sample ID"], ena_IDs))
+    return result
+
+
+def load_pacb_ilmn_pf(tsv_fname: str) -> List[ENARecord]:
+    result = list()
+    with open(tsv_fname) as tsvfile:
+        reader = DictReader(tsvfile, delimiter="\t")
+        for row in reader:
+            # Filter out PfML01, as it requires too much memory for cortex to process (with mem_height=22)
+            if row["sample_name"].startswith("#") or row["sample_name"] in {
+                "Pf3D7",
+                "PfML01",
+            }:
+                continue
+            ena_IDs = row["ENA_sample_accession_ILMN"]
+            result.append(ENARecord("pacb_ilmn_pf", row["sample_name"], ena_IDs))
+    return result
 
 
 def find_reads(wildcards) -> List[str]:
