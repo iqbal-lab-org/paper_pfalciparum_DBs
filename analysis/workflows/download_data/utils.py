60373849f3ddb7a8874b29dda115b2c89790d0ab
Brice Letcher <bletcher@ebi.ac.uk>
2021-05-27 11:41:25 +0100

    Download malariaGEN vcfs in download_data

---
+++ analysis/workflows/download_data/utils.py
@@ -0,0 +1,5 @@
+def get_vcf_url(wildcards):
+    if wildcards.dataset_name == "pf6":
+        url_template=config["vcf"]["pf6_ftp"] + config["vcf"]["pf6_fname_template"]
+        return url_template.format(chrom=wildcards.chrom)
+    raise ValueError(f"Unsupported dataset name: {wildcards.dataset_name}")
