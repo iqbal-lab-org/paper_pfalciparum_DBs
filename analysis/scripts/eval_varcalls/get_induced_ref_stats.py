d2fbe6b0c1a2377f7a28523bec2a566f48e41fee
Brice Letcher <bletcher@ebi.ac.uk>
2021-11-16 15:08:30 +0000

    New stats in eval_varcalls
    * Fraction of positions in a region where majority of pileup disagrees with ref base
    * Fraction of pileup positions under a given coverage
    * With unit tests

--- analysis/scripts/eval_varcalls/get_induced_ref_stats.py
+++ analysis/scripts/eval_varcalls/get_induced_ref_stats.py
@@ -1,10 +1,10 @@
 import sys
 from pathlib import Path
+import re
 
-from pysam import VariantFile, stats as sam_stats
-from edlib import align as edalign
+from pysam import VariantFile, stats as sam_stats, mpileup as sam_mpileup
 
-from shift_to_induced_genome_coords import translate_bed
+from eval_varcalls.shift_to_induced_genome_coords import translate_bed
 
 
 def usage():
@@ -29,6 +29,9 @@ class Stats:
         "reads_properly_paired",
         "bases_mapped_cigar",
         "base_error_rate",
+        "fraction_disagreeing_pileup",
+        "fraction_positions_0x_or_less",
+        "fraction_positions_9x_or_less",
     ]
 
     def __init__(self, sample_name: str, tool_name: str, gene_name: str):
@@ -51,6 +54,103 @@ class Stats:
 
     def __repr__(self):
         return "\t".join(map(str, [self.gene[key] for key in self.attributes]))
+
+
+def populate_stats(record, input_bam, region) -> None:
+    # Obtain mapping statistics in bed region via bam
+    stats = sam_stats(str(input_bam), region, split_lines=True)
+    for stat_line in stats:
+        if not stat_line.startswith("SN"):
+            continue
+        if "reads mapped and paired" in stat_line:
+            record["reads_mapped_and_paired"] = int(stat_line.split()[5])
+        elif "reads properly paired" in stat_line:
+            record["reads_properly_paired"] = int(stat_line.split()[4])
+        elif "bases mapped (cigar):" in stat_line:
+            record["bases_mapped_cigar"] = int(stat_line.split()[4])
+        elif "error rate" in stat_line:
+            record["base_error_rate"] = float(stat_line.split()[3])
+        else:
+            continue
+
+
+PILEUP_INDEL_RE = "[+-]([0-9]+)[ACGTNacgtn*#]+"
+PILEUP_MATCH = ".,"
+PILEUP_MISMATCH = "ATCGNatcgn*"
+
+pileup_matches = lambda char: char in PILEUP_MATCH
+pileup_mismatches = lambda char: char in PILEUP_MISMATCH
+
+
+def remove_indels(pileup_string: str) -> str:
+    """
+    See man samtools-mpileup for what the pileup column can contain. Here I remove indels, making sure that no more than the indel is clipped out.
+    E.g.
+    Input:  '.,2aca,.' # the second a is a mismatch, not part of the insertion description
+    Output: '.,a,.'
+    """
+    new_string = ""
+    cur_pos = 0
+    for match in re.finditer(PILEUP_INDEL_RE, pileup_string):
+        indel_length = int(match.group(1))
+        new_string += pileup_string[cur_pos : match.start(0)]
+        cur_pos = match.end(1) + indel_length
+    new_string += pileup_string[cur_pos:]
+    return new_string
+
+
+def remove_read_ends(pileup_string: str) -> str:
+    new_string = ""
+    cur_pos = 0
+    for match in re.finditer(r"(\^.)|(\$)", pileup_string):
+        new_string += pileup_string[cur_pos : match.start(0)]
+        cur_pos = match.end(0)
+    new_string += pileup_string[cur_pos:]
+    return new_string
+
+
+def majority_pileup_is_non_ref(pileup) -> bool:
+    matches = sum(map(pileup_matches, pileup))
+    mismatches = sum(map(pileup_mismatches, pileup))
+    frac_matches = matches / (matches + mismatches)
+    return frac_matches < 0.5
+
+
+def get_fraction_below_depth(depths, limit):
+    num_below = len([depth for depth in depths if depth <= limit])
+    return round(num_below / len(depths), 3)
+
+
+def populate_pileup(record, input_bam, input_ref_genome, region) -> None:
+    """
+    Computes the fraction of positions that do not match, and fraction of positions under a given depth
+    """
+    num_disagreeing, num_total = 0, 0
+    mpileup_output = sam_mpileup(
+        str(input_bam),
+        "-r",
+        region,
+        "-f",
+        str(input_ref_genome),
+        "-a",
+        split_lines=True,
+    )
+    pileups = map(
+        lambda line: remove_read_ends(remove_indels(line.split("\t")[4])),
+        mpileup_output,
+    )
+    depths = list(map(lambda line: int(line.split("\t")[3]), mpileup_output))
+    for pileup, depth in zip(pileups, depths):
+        if depth == 0:
+            continue
+        num_total += 1
+        if majority_pileup_is_non_ref(pileup):
+            num_disagreeing += 1
+    record["fraction_disagreeing_pileup"] = round(num_disagreeing / num_total, 3)
+    for limit in [0, 9]:
+        record[f"fraction_positions_{limit}x_or_less"] = get_fraction_below_depth(
+            depths, limit
+        )
 
 
 if __name__ == "__main__":
@@ -97,21 +197,7 @@ if __name__ == "__main__":
         record = Stats(sample_name, tool_name, gene_name)
         region = f"{bed_line[0]}:{reg_start}-{reg_end}"
 
-        # Obtain mapping statistics in bed region via bam
-        stats = sam_stats(str(input_bam), region, split_lines=True)
-        for stat_line in stats:
-            if not stat_line.startswith("SN"):
-                continue
-            if "reads mapped and paired" in stat_line:
-                record["reads_mapped_and_paired"] = int(stat_line.split()[5])
-            elif "reads properly paired" in stat_line:
-                record["reads_properly_paired"] = int(stat_line.split()[4])
-            elif "bases mapped (cigar):" in stat_line:
-                record["bases_mapped_cigar"] = int(stat_line.split()[4])
-            elif "error rate" in stat_line:
-                record["base_error_rate"] = float(stat_line.split()[3])
-            else:
-                continue
-
+        populate_stats(record, input_bam, region)
+        populate_pileup(record, input_bam, input_ref_genome, region)
         print(record, file=output_file)
     output_file.close()
---
+++ analysis/scripts/tests/test_get_induced_ref_stats.py
@@ -0,0 +1,41 @@
+from eval_varcalls.get_induced_ref_stats import remove_indels,remove_read_ends,majority_pileup_is_non_ref,get_fraction_below_depth
+
+class TestFractionBelowDepth:
+    def test_all_zerodepth(self):
+        depths = [0, 0]
+        assert get_fraction_below_depth(depths, 0) == 1
+        assert get_fraction_below_depth(depths, 1) == 1
+
+    def test_mix_of_depths(self):
+        depths = [0,1,2,3]
+        assert get_fraction_below_depth(depths, 0) == 0.25
+        assert get_fraction_below_depth(depths, 2) == 0.75
+
+class TestProcessPileup:
+    def test_pileup_with_indels(self):
+        pileup_string = ",.-2Aa...G"
+        result = remove_indels(pileup_string)
+        assert result == ",....G"
+
+    def test_pileup_with_indels_next_to_mismatches(self):
+        pileup_string = ",.-2AAAAAA+1Tt."
+        result = remove_indels(pileup_string)
+        assert result == ",.AAAAt."
+
+    def test_pileup_with_indels_and_read_starts_and_ends(self):
+        pileup_string = ",.-2AA.G^a$.^]AT"
+        result = remove_read_ends(remove_indels(pileup_string))
+        assert result == ",..G.AT"
+
+class TestMajorityPileup:
+    def test_equal_match_mismatch(self):
+        pileup_string = ",.AAa,"
+        assert not majority_pileup_is_non_ref(pileup_string)
+
+    def test_majority_match(self):
+        pileup_string = ",....A,"
+        assert not majority_pileup_is_non_ref(pileup_string)
+
+    def test_majority_mismatch(self):
+        pileup_string = ".AAatcAA,"
+        assert majority_pileup_is_non_ref(pileup_string)
