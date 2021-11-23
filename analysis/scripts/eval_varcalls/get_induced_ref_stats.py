b5baf25a9569b53b8d8c922c0d348ab4dc5f80a4
Brice Letcher <bletcher@ebi.ac.uk>
2021-11-23 15:06:45 +0000

    Eval_varcalls: add many more stats
    In evaluation using induced ref genome, now compute stats relating
    to read MAPQ, read insert size, and consensus pileup.

--- analysis/scripts/eval_varcalls/get_induced_ref_stats.py
+++ analysis/scripts/eval_varcalls/get_induced_ref_stats.py
@@ -1,10 +1,23 @@
 import sys
 from pathlib import Path
+from typing import Tuple
 import re
 
-from pysam import VariantFile, stats as sam_stats, mpileup as sam_mpileup
+import click
+from pysam import (
+    AlignmentFile,
+    stats as sam_stats,
+    mpileup as sam_mpileup,
+)
 
 from eval_varcalls.shift_to_induced_genome_coords import translate_bed
+
+
+def print_header(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    print("\t".join(Stats.attributes), file=sys.stdout)
+    ctx.exit()
 
 
 def usage():
@@ -25,13 +38,20 @@ class Stats:
         "sample",
         "tool",
         "gene",
-        "reads_mapped_and_paired",
-        "reads_properly_paired",
         "bases_mapped_cigar",
         "base_error_rate",
         "fraction_disagreeing_pileup",
         "fraction_positions_0x_or_less",
         "fraction_positions_9x_or_less",
+        "fraction_reads_paired",
+        "fraction_reads_mapped_and_paired",
+        "fraction_reads_paired_one_unmapped",
+        "fraction_reads_mapped_and_paired_mapq_1_or_more",
+        "fraction_reads_mapped_and_paired_mapq_10_or_more",
+        "fraction_reads_mapped_and_paired_mapq_30_or_more",
+        "fraction_reads_properly_paired_aligner",
+        "fraction_reads_above_mean_ins_size_plus_two_std",
+        "fraction_reads_below_mean_ins_size_minus_two_std",
     ]
 
     def __init__(self, sample_name: str, tool_name: str, gene_name: str):
@@ -56,17 +76,28 @@ class Stats:
         return "\t".join(map(str, [self.gene[key] for key in self.attributes]))
 
 
-def populate_stats(record, input_bam, region) -> None:
+def extract_read_inserts_SN(input_bam, bed_fname) -> Tuple[float, float]:
+    stats = sam_stats(str(input_bam), "-t",bed_fname, split_lines=True)
+    mean, std = None, None
+    for stat_line in stats:
+        if not stat_line.startswith("SN"):
+            continue
+        if "insert size average" in stat_line:
+            mean = float(stat_line.split()[4])
+        if "insert size standard deviation" in stat_line:
+            std = float(stat_line.split()[5])
+        if mean is not None and std is not None:
+            break
+    return mean, std
+
+
+def populate_stats_SN(record, input_bam, region) -> None:
     # Obtain mapping statistics in bed region via bam
     stats = sam_stats(str(input_bam), region, split_lines=True)
     for stat_line in stats:
         if not stat_line.startswith("SN"):
             continue
-        if "reads mapped and paired" in stat_line:
-            record["reads_mapped_and_paired"] = int(stat_line.split()[5])
-        elif "reads properly paired" in stat_line:
-            record["reads_properly_paired"] = int(stat_line.split()[4])
-        elif "bases mapped (cigar):" in stat_line:
+        if "bases mapped (cigar):" in stat_line:
             record["bases_mapped_cigar"] = int(stat_line.split()[4])
         elif "error rate" in stat_line:
             record["base_error_rate"] = float(stat_line.split()[3])
@@ -116,12 +147,15 @@ def majority_pileup_is_non_ref(pileup) -> bool:
     return frac_matches < 0.5
 
 
-def get_fraction_below_depth(depths, limit):
-    num_below = len([depth for depth in depths if depth <= limit])
-    return round(num_below / len(depths), 3)
+def get_fraction_leq_limit(values, limit):
+    """
+    leq: less than or equal to
+    """
+    num_below = len([value for value in values if value <= limit])
+    return round(num_below / len(values), 3)
 
 
-def populate_pileup(record, input_bam, input_ref_genome, region) -> None:
+def populate_stats_pileup(record, input_bam, input_ref_genome, region) -> None:
     """
     Computes the fraction of positions that do not match, and fraction of positions under a given depth
     """
@@ -148,56 +182,108 @@ def populate_pileup(record, input_bam, input_ref_genome, region) -> None:
             num_disagreeing += 1
     record["fraction_disagreeing_pileup"] = round(num_disagreeing / num_total, 3)
     for limit in [0, 9]:
-        record[f"fraction_positions_{limit}x_or_less"] = get_fraction_below_depth(
+        record[f"fraction_positions_{limit}x_or_less"] = get_fraction_leq_limit(
             depths, limit
         )
 
 
-if __name__ == "__main__":
-    try:
-        if sys.argv[1] == "--header":
-            Stats.print_header()
-            exit(0)
-    except IndexError:
-        pass
+def populate_stats_reads(
+    record, input_bam, chrom, start, end, insert_mean: float, insert_std: float
+) -> None:
+    alignment_file = AlignmentFile(input_bam)
+    read_stats = {metric: 0 for metric in Stats.attributes[8:15]}
+    mapqs = list()
+    tlens = list()
+    for num_reads, read in enumerate(alignment_file.fetch(chrom, start, end)):
+        read_paired = read.is_paired
+        read_mapped = not read.is_unmapped
+        mate_mapped = not read.mate_is_unmapped
+        if read_paired:
+            read_stats["fraction_reads_paired"] += 1
+        if read_paired and read_mapped and mate_mapped:
+            read_stats["fraction_reads_mapped_and_paired"] += 1
+            mapqs.append(read.mapping_quality)
+            tlens.append(read.tlen)
+        if read_paired and read.is_proper_pair and read_mapped:
+            read_stats["fraction_reads_properly_paired_aligner"] += 1
+        if read_paired and read_mapped and not mate_mapped:
+            read_stats["fraction_reads_paired_one_unmapped"] += 1
+    for metric in read_stats:
+        read_stats[metric] = round(read_stats[metric] / (num_reads + 1), 3)
+    for limit in [0, 9, 29]:
+        metric_name = f"fraction_reads_mapped_and_paired_mapq_{limit + 1}_or_more"
+        read_stats[metric_name] = round(1 - get_fraction_leq_limit(mapqs, limit), 3)
+    for metric in read_stats:
+        record[metric] = read_stats[metric]
+    if insert_mean is not None:
+        record[
+            "fraction_reads_above_mean_ins_size_plus_two_std"
+        ] = round(1 - get_fraction_leq_limit(tlens, insert_mean + 2 * insert_std), 3)
+        record[
+            "fraction_reads_below_mean_ins_size_minus_two_std"
+        ] = get_fraction_leq_limit(tlens, insert_mean - 2 * insert_std)
 
-    if len(sys.argv) != 7:
-        usage()
 
-    input_bed = Path(sys.argv[1]).resolve()
-    input_ref_genome = Path(sys.argv[2]).resolve()
-    input_vcf = Path(sys.argv[3]).resolve()
-    input_bam = Path(sys.argv[4]).resolve()
-    output_tsv = Path(sys.argv[5]).resolve()
-    metadata = sys.argv[6]
+@click.command()
+@click.argument("bed_fname", type=click.Path(exists=True))
+@click.argument("ref_fname", type=click.Path(exists=True))
+@click.argument("vcf_fname", type=click.Path(exists=True))
+@click.argument("bam_fname", type=click.Path(exists=True))
+@click.option("--bed_for_insert_size", "-bi", type=click.Path(exists=True))
+@click.option("--sample_name", required=True)
+@click.option("--tool_name", required=True)
+@click.option(
+    "--out_fname",
+    type=click.File("w"),
+    default=sys.stdout,
+    help="File to write to. Default: stdout",
+)
+@click.option(
+    "--header_only",
+    is_flag=True,
+    help="Print the tsv header only",
+    is_eager=True,
+    callback=print_header,
+)
+def main(
+    bed_fname,
+    ref_fname,
+    vcf_fname,
+    bam_fname,
+    bed_for_insert_size,
+    out_fname,
+    header_only,
+    sample_name,
+    tool_name,
+):
+    if not Path(f"{vcf_fname}.csi").exists():
+        raise Exception(f"bcftools index for {vcf_fname} not found")
 
-    for fname in (input_bed, input_vcf, input_ref_genome, input_bam):
-        if not fname.exists():
-            print(f"Error: {fname} required but not found")
-            usage()
+    translated_bed = translate_bed(Path(bed_fname), ref_fname, vcf_fname)
 
-    try:
-        sample_name, tool_name = metadata.split(":")
-    except ValueError:
-        print("Metadata formatting error")
-        usage()
+    log_file = Path(str(out_fname)).with_suffix(".translated_bed.txt").open("w")
 
-    vcf_file = VariantFile(str(input_vcf))
-    if not Path(f"{input_vcf}.csi").exists():
-        raise Exception(f"bcftools index for {input_vcf} not found")
-
-    translated_bed = translate_bed(input_bed, input_ref_genome, input_vcf)
-
-    output_file = output_tsv.open("w")
+    insert_mean, insert_std = None, None
+    if bed_for_insert_size is not None:
+        insert_mean, insert_std = extract_read_inserts_SN(
+            bam_fname, bed_for_insert_size
+        )
 
     for bed_line in translated_bed:
-        gene_name = bed_line[3]
+        log_file.write("\t".join(bed_line))
+        gene_name = bed_line[3].strip()
         reg_start = int(bed_line[1]) + 1
         reg_end = int(bed_line[2])
         record = Stats(sample_name, tool_name, gene_name)
         region = f"{bed_line[0]}:{reg_start}-{reg_end}"
 
-        populate_stats(record, input_bam, region)
-        populate_pileup(record, input_bam, input_ref_genome, region)
-        print(record, file=output_file)
-    output_file.close()
+        populate_stats_SN(record, bam_fname, region)
+        populate_stats_pileup(record, bam_fname, ref_fname, region)
+        populate_stats_reads(
+            record, bam_fname, bed_line[0], reg_start, reg_end, insert_mean, insert_std
+        )
+        print(record, file=out_fname)
+
+
+if __name__ == "__main__":
+    main()
--- analysis/scripts/tests/test_get_induced_ref_stats.py
+++ analysis/scripts/tests/test_get_induced_ref_stats.py
@@ -1,15 +1,22 @@
-from eval_varcalls.get_induced_ref_stats import remove_indels,remove_read_ends,majority_pileup_is_non_ref,get_fraction_below_depth
+from eval_varcalls.get_induced_ref_stats import (
+    remove_indels,
+    remove_read_ends,
+    majority_pileup_is_non_ref,
+    get_fraction_leq_limit,
+)
+
 
 class TestFractionBelowDepth:
     def test_all_zerodepth(self):
         depths = [0, 0]
-        assert get_fraction_below_depth(depths, 0) == 1
-        assert get_fraction_below_depth(depths, 1) == 1
+        assert get_fraction_leq_limit(depths, 0) == 1
+        assert get_fraction_leq_limit(depths, 1) == 1
 
     def test_mix_of_depths(self):
-        depths = [0,1,2,3]
-        assert get_fraction_below_depth(depths, 0) == 0.25
-        assert get_fraction_below_depth(depths, 2) == 0.75
+        depths = [0, 1, 2, 3]
+        assert get_fraction_leq_limit(depths, 0) == 0.25
+        assert get_fraction_leq_limit(depths, 2) == 0.75
+
 
 class TestProcessPileup:
     def test_pileup_with_indels(self):
@@ -26,6 +33,7 @@ class TestProcessPileup:
         pileup_string = ",.-2AA.G^a$.^]AT"
         result = remove_read_ends(remove_indels(pileup_string))
         assert result == ",..G.AT"
+
 
 class TestMajorityPileup:
     def test_equal_match_mismatch(self):
