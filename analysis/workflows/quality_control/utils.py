02dc43b4a41396b1ba1abd1afc7c4c944ca162d4
Brice Letcher <bletcher@ebi.ac.uk>
2021-11-30 15:55:11 +0000

    pacb_ilmn_samples fixes
    * Add them to quality_control workflow
    * Update varifier to fixed bug for float COV in gramtools VCF

--- analysis/workflows/quality_control/utils.py
+++ analysis/workflows/quality_control/utils.py
@@ -8,7 +8,7 @@ def qc_get_tool_vcf(wildcards):
         ["cortex", "octopus", "gram_adju"],
     )
     if any(varcall_tool):
-        dataset_name = wildcards.dataset_name.split("_")[0]
+        dataset_name = "pf6" if wildcards.dataset_name.startswith("pf6") else wildcards.dataset_name
         tool_path = f'{config["varcall_dir"]}/{wildcards.tool}/{dataset_name}/{wildcards.sample_name}/'
         if wildcards.tool.startswith("gram"):
             tool_path += "final.vcf.gz"
@@ -30,12 +30,12 @@ def qc_get_expected_vcf_stats(wildcards):
     Cf `qc_get_one_vcf` for how tool names in this function map to vcf output file names.
     """
     if wildcards.dataset_name.startswith("pf6"):
-        tools = [
-            gram_adju,
-            f"{gram_jointgeno}__{wildcards.dataset_name}__7__13",
-        ]
+        tools = [gram_adju, f"{gram_jointgeno}__{wildcards.dataset_name}__7__13"]
         samples = cu_record_to_sample_names(cu_load_pf6(config["pf6_validation_tsv"]))
         #samples = cu_get_sample_names_fws_matching(wildcards.dataset_name)
+    elif wildcards.dataset_name.startswith("pacb_ilmn_pf"):
+        tools = [gram_adju, f"{gram_jointgeno}__{wildcards.dataset_name}@pf6_analysis_set_fws95__7__13"]
+        samples = cu_record_to_sample_names(cu_load_pacb_ilmn_pf(config["pacb_ilmn_pf_tsv"]))
     else:
         raise ValueError(f"Unsupported dataset name: {wildcards.dataset_name}")
     return expand(
@@ -46,16 +46,19 @@ def qc_get_expected_vcf_stats(wildcards):
 
 
 def qc_get_mapped_reads(wildcards):
-    dataset_name = wildcards.dataset_name.split("_")[0]
+    dataset_name = "pf6" if wildcards.dataset_name.startswith("pf6") else wildcards.dataset_name
     return f'{config["varcall_dir"]}/mapped_reads/{dataset_name}/{wildcards.sample_name}/mapped.bam'
 
 def qc_get_expected_read_stats(wildcards):
+    dataset_name = wildcards.dataset_name
     if wildcards.dataset_name.startswith("pf6"):
         samples = cu_record_to_sample_names(cu_load_pf6(config["pf6_validation_tsv"]))
+        dataset_name = "pf6"
         #samples = cu_get_sample_names_fws_matching(wildcards.dataset_name)
+    elif wildcards.dataset_name.startswith("pacb_ilmn_pf"):
+        samples = cu_record_to_sample_names(cu_load_pacb_ilmn_pf(config["pacb_ilmn_pf_tsv"]))
     else:
         raise ValueError(f"Unsupported dataset name: {wildcards.dataset_name}")
-    dataset_name = wildcards.dataset_name.split("_")[0]
     return expand(
         f"{output_read_stats}/per_sample/{dataset_name}/{{sample_name}}_read_stats.tsv",
         sample_name=samples,
