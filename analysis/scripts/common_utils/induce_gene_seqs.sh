fd1fdd94fcc4709e990d5ba4fef24765e1345a4f
Brice Letcher <bletcher@ebi.ac.uk>
2022-01-17 14:37:47 +0000

    Refactor eval_varcalls slightly

--- analysis/scripts/common_utils/induce_gene_seqs.sh
+++ analysis/scripts/common_utils/induce_gene_seqs.sh
@@ -1,17 +1,19 @@
 usage (){
-echo "usage: $0 bed_fname vcf_fname ref_genome_fname outdir_fname sample_name"
+echo "usage: $0 bed_fname vcf_fname ref_genome_fname outdir_fname sample_name [1|0]"
 echo "Takes a bed, a vcf, and a reference genome, and outputs each sequence from each feature in the bed with variants applied, to files in 'outdir_fname'. sample_name gets placed in output fasta headers."
+echo "The last param is 1 for placing sample_name in the header of the output sequences or 0 for not. Default 1"
 exit 1
 }
 
-if [[ "$#" -ne 5 ]]; then usage ;fi
+echo "$#"
+if [ $# != "5" -a "$#" != "6" ]; then usage ;fi
 
 bed_fname="$1"
 vcf_fname="$2"
 ref_genome_fname="$3"
 outdir_fname="$4"
 sample_name="$5"
-
+sname_in_output="${6:-1}"
 
 mkdir -p "$outdir_fname"
 cp "$vcf_fname" input.vcf.gz
@@ -23,6 +25,8 @@ do
     reg="${elems[0]}:${adj_start}-${elems[2]}"
     gene_name=${elems[3]}
     fout=${outdir_fname}/${gene_name}.fa
-    samtools faidx "$ref_genome_fname" $reg | bcftools consensus input.vcf.gz |
-        sed 's/>.*/>'"${gene_name}_${sample_name}"'/' > $fout
+    new_name="$gene_name"
+    if [ $sname_in_output -eq 1 ]; then new_name="${new_name}_${sample_name}"; fi
+    samtools faidx "$ref_genome_fname" $reg | bcftools consensus -s "$sample_name" input.vcf.gz |
+        sed 's/>.*/>'"$new_name"'/' > $fout
 done
