72c7e2dd50b67c7e2c586f3b796b56db0ef5f35c
Brice Letcher <bletcher@ebi.ac.uk>
2022-01-28 21:44:18 +0000

    Hotfix: allow induce gene seqs to apply all sample variants in vcf

--- analysis/scripts/common_utils/induce_gene_seqs.sh
+++ analysis/scripts/common_utils/induce_gene_seqs.sh
@@ -1,12 +1,12 @@
 usage (){
-echo "usage: $0 bed_fname vcf_fname ref_genome_fname outdir_fname sample_name [1|0]"
+echo "usage: $0 bed_fname vcf_fname ref_genome_fname outdir_fname sample_name [1|0] [1|0]"
 echo "Takes a bed, a vcf, and a reference genome, and outputs each sequence from each feature in the bed with variants applied, to files in 'outdir_fname'. sample_name gets placed in output fasta headers."
-echo "The last param is 1 for placing sample_name in the header of the output sequences or 0 for not. Default 1"
+echo "The penultimate param is 1 for placing sample_name in the header of the output sequences or 0 for not. Default 1"
+echo "The ultimate param is 1 for applying all calls in the vcf, ignoring sample_name. Useful for template vcf with sample_name=sample. Default 0."
 exit 1
 }
 
-echo "$#"
-if [ $# != "5" -a "$#" != "6" ]; then usage ;fi
+if [ $# != "5" -a "$#" != "6" -a "$#" != "7" ]; then usage ;fi
 
 bed_fname="$1"
 vcf_fname="$2"
@@ -14,10 +14,13 @@ ref_genome_fname="$3"
 outdir_fname="$4"
 sample_name="$5"
 sname_in_output="${6:-1}"
+apply_all_samples="${7:-0}"
 
 mkdir -p "$outdir_fname"
 cp "$vcf_fname" input.vcf.gz
 bcftools index input.vcf.gz
+bcftools_sample="-s $sample_name"
+if [ $apply_all_samples -eq 1 ]; then bcftools_sample="";fi
 IFS=$'\n'; for gene_line in $(cat "$bed_fname")
 do
     IFS=$'\t'; elems=($gene_line)    
@@ -27,6 +30,6 @@ do
     fout=${outdir_fname}/${gene_name}.fa
     new_name="$gene_name"
     if [ $sname_in_output -eq 1 ]; then new_name="${new_name}_${sample_name}"; fi
-    samtools faidx "$ref_genome_fname" $reg | bcftools consensus -s "$sample_name" input.vcf.gz |
+    samtools faidx "$ref_genome_fname" $reg | bcftools consensus input.vcf.gz $bcftools_sample |
         sed 's/>.*/>'"$new_name"'/' > $fout
 done
