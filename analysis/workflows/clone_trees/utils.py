83ab1e04573e47b3f83eba23a8d2f4d962e7f8cc
Brice Letcher <bletcher@ebi.ac.uk>
2022-03-08 17:59:06 +0000

    Clone trees (2): add gapfiller
    * Add bed translation, at each successive induced ref production step
    * Add gapfiller rule + use gram adju's bed in gapfiller rule
    * Adapt gapfiller running in call_variants workflow to simplify the clone tree workflow logic
     (I removed gapfiller rule production of a translated bed)

--- analysis/workflows/clone_trees/utils.py
+++ analysis/workflows/clone_trees/utils.py
@@ -8,5 +8,17 @@ def ct_get_ref_genome(wildcards):
         return cu_get_ref_genome(wildcards)
     elif wildcards.tool.startswith("gram_adju"):
         return f"{output_gram_joint_geno}/{wildcards.sample_name}/induced_ref.fa.gz", 
+    elif wildcards.tool.startswith("gapfiller"):
+        return f"{output_gram_adju}/{wildcards.sample_name}/induced_ref.fa.gz", 
+    else:
+        raise ValueError(f"Unsupported tool: {wildcards.tool}")
+
+def ct_get_translation_bed(wildcards):
+    if wildcards.tool.startswith("gram_jointgeno"):
+        return GENE_BED
+    elif wildcards.tool.startswith("gram_adju"):
+        return f"{output_gram_joint_geno}/{wildcards.sample_name}/{GENE_LIST_NAME}_induced.bed",
+    elif wildcards.tool.startswith("gapfiller"):
+        return f"{output_gram_adju}/{wildcards.sample_name}/{GENE_LIST_NAME}_induced.bed",
     else:
         raise ValueError(f"Unsupported tool: {wildcards.tool}")
