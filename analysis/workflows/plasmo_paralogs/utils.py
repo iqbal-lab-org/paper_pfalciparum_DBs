02408bef5d36bcbf8939c555211cbfc352527ff5
Brice Letcher <bletcher@ebi.ac.uk>
2022-04-04 14:41:03 +0100

    Plasmo paralogs: induce all gene seqs in one go
    And induce all genes, not just paralogs

--- analysis/workflows/plasmo_paralogs/utils.py
+++ analysis/workflows/plasmo_paralogs/utils.py
@@ -15,11 +15,33 @@ def pp_filter_to_paralogs(genes: List[str]) -> List[str]:
     return list(filter(lambda gene: gene in genes_of_interest, genes))
 
 
-def pp_get_one_vcf(wildcards):
-    if wildcards.sample_name == "ref":
+def pp_get_all_vcfs(wildcards, mode):
+    result = list()
+    sample_names = _pp_load_sample_names(wildcards)
+    for sample_name in sample_names:
+        if mode == "sample_name":
+            result.append(sample_name)
+        elif mode == "vcf":
+            result.append(_pp_get_one_vcf(wildcards, sample_name))
+        else:
+            raise ValueError(f"Unsupported mode: {mode}")
+    return result
+
+def _pp_load_sample_names(wildcards):
+    if wildcards.sample_set_name == "pf6_analysis_set_fws95":
+        result = ["ref"] + cu_get_sample_names_fws_matching(wildcards.sample_set_name)
+    elif wildcards.sample_set_name == "pacb_ilmn_pf@pf6_analysis_set_fws95":
+        result = cu_record_to_sample_names(cu_load_pacb_ilmn_pf(config["pacb_ilmn_pf_tsv"]))
+    else:
+        raise ValueError(f"Unsupported sample set name: {wildcads.sample_set_name}")
+    return result
+
+
+def _pp_get_one_vcf(wildcards, sample_name):
+    if sample_name == "ref":
         return f'{config["input_data"]}/template.vcf.gz'
     if wildcards.sample_set_name == "pf6_analysis_set_fws95":
-        return f'{config["jointgeno_dir"]}/gram_jointgeno_{GMTOOLS_COMMIT}/pf6/analysis_set_fws95/{wildcards.tool}/pf6_26_genes_7_13/{wildcards.sample_name}/final.vcf.gz'
+        return f'{config["jointgeno_dir"]}/gram_jointgeno_{GMTOOLS_COMMIT}/pf6/analysis_set_fws95/{wildcards.tool}/pf6_26_genes_7_13/{sample_name}/final.vcf.gz'
     if wildcards.sample_set_name == "pacb_ilmn_pf@pf6_analysis_set_fws95":
-        return f'{config["jointgeno_dir"]}/gram_jointgeno_{GMTOOLS_COMMIT}/pacb_ilmn_pf@pf6/analysis_set_fws95/{wildcards.tool}/pf6_26_genes_7_13/{wildcards.sample_name}/final.vcf.gz'
+        return f'{config["jointgeno_dir"]}/gram_jointgeno_{GMTOOLS_COMMIT}/pacb_ilmn_pf@pf6/analysis_set_fws95/{wildcards.tool}/pf6_26_genes_7_13/{sample_name}/final.vcf.gz'
     raise ValueError(f"Unsupported set of wildcards: {wildcards}")
